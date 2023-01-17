85b81d330191af86afbce688661db0103a8a2559
Julia Rubina <jl_rubina@mail.ru>
2023-01-17 12:00:17 +0300

    initial commit

---
+++ src/main/java/ru/netology/api/Post.java
@@ -0,0 +1,11 @@
+package ru.netology.api;
+
+public class Post {
+    public FormDate birthday;
+    public String name;
+    public String passport;
+    public String patronimic;
+    public String phone;
+    public String surname;
+    public boolean subscription;
+}
